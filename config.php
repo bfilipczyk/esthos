4df3bf1d2def4fd4497433d0e9514291439a87f3
bfilipczyk <bfilipczyk119@gmail.com>
2021-01-12 22:13:54 +0100

    Clean up ignored files

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'rcjstqlqdliivb';
-const PASSWORD = 'b84821c7abccf6e4e9d47abb1503e4dcaa23f71e94f97a5017b026dbe22049d9';
-const HOST = 'ec2-79-125-59-247.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd3rrebetkf6cje';
